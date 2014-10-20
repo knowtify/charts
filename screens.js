b3714abafff0be13b8f652b9d393063643360b5f
Dane Lyons <duilen@gmail.com>
2014-10-20 16:41:25 -0700

    home

--- screens.js
+++ screens.js
@@ -1,3 +1,3 @@
 exports.home = function (req, res) {
-  res.send('<h1>Analytics for <a href="http://www.knowtify.io">Knowtify.io</a></h1>');
+  res.send('<h1>Charts for <a href="http://www.knowtify.io">Knowtify.io</a></h1>');
 }
