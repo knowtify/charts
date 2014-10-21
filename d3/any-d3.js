ef9e66fc95b666f404753344116cddb959675377
Dane Lyons <duilen@gmail.com>
2014-10-21 12:51:02 -0700

    d3

--- d3/any-d3.js
+++ d3/any-d3.js
@@ -1,3 +1,6 @@
+$.getScript('http://s3.amazonaws.com/js.knowtify.io/js/demo/sunburst.js', function( data, textStatus, jqxhr ) {
+	  	});
+
 function plot(params){
 	$('body').append($('<div id="'+params.chart_id+'">'+params.d3_script+'</div>'));
 	/*
