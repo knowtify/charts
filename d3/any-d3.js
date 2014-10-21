501c8ae622496d3e4c012d2c84993875ec4444e2
Dane Lyons <duilen@gmail.com>
2014-10-21 12:58:44 -0700

    d3

--- d3/any-d3.js
+++ d3/any-d3.js
@@ -1,16 +1,6 @@
-$.getScript('http://s3.amazonaws.com/js.knowtify.io/js/demo/sunburst.js', function( data, textStatus, jqxhr ) {
-	  	});
-
 function plot(params){
 	$('body').append($('<div id="'+params.chart_id+'">'+params.d3_script+'</div>'));
-	/*
+	$.ajaxSetup({async:false});
 	$.getScript(params.d3_script, function( data, textStatus, jqxhr ) {
-	  
 	});
-	*/
-	//http://s3.amazonaws.com/js.knowtify.io/js/demo/sunburst.js
-	setTimeout(function(){
-		$.getScript(params.d3_script, function( data, textStatus, jqxhr ) {
-	  	});
-	},500);
 }
