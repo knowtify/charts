f06fd878c606ab0b97fb1a80666a4037c76c99da
Dane Lyons <duilen@gmail.com>
2014-10-21 12:48:39 -0700

    d3

--- d3/any-d3.js
+++ d3/any-d3.js
@@ -1,5 +1,5 @@
 function plot(params){
-	$('body').append($('<div id="'+params.chart_id+'">'+params.chart_id+'</div>'));
+	$('body').append($('<div id="'+params.chart_id+'">'+params.d3_script+'</div>'));
 	/*
 	$.getScript(params.d3_script, function( data, textStatus, jqxhr ) {
 	  
